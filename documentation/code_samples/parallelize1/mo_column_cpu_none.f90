845becfabe9186422640beb3a9022efdf9203b77
Valentin Clement <clementval@gmail.com>
2016-10-24 09:35:13 +0200

    Update openmp code sample

--- documentation/code_samples/parallelize1/mo_column_cpu_none.f90
+++ documentation/code_samples/parallelize1/mo_column_cpu_none.f90
@@ -7,25 +7,36 @@ CONTAINS
   REAL , INTENT(INOUT) :: q ( : , : )
   INTEGER :: k
   REAL :: c
+  REAL :: d ( 1 : nproma )
   INTEGER , INTENT(IN) :: nproma
   INTEGER :: proma
-  REAL :: d ( 1 : nproma )
 
+!$omp parallel
   c = 5.345
   DO k = 2 , nz , 1
+!$omp do
    DO proma = 1 , nproma , 1
     t ( proma , k ) = c * k
    END DO
+!$omp end do
+!$omp do
    DO proma = 1 , nproma , 1
-    d ( proma ) = t ( proma , k ) ** 2
+    d ( proma ) = t ( proma , k ) ** ( 2 )
    END DO
+!$omp end do
+!$omp do
    DO proma = 1 , nproma , 1
     q ( proma , k ) = q ( proma , k - 1 ) + t ( proma , k ) * c
    END DO
+!$omp end do
   END DO
+!$omp do
   DO proma = 1 , nproma , 1
    q ( proma , nz ) = q ( proma , nz ) * c
   END DO
+!$omp end do
+!$omp end parallel
  END SUBROUTINE compute_column
 
 END MODULE mo_column
+
